7a1680fb2d162466d96a8aa2810ca3f89476deaa
Percy Molina Abad <extpmolina@belcorp.biz>
2024-05-31 02:07:57 -0500

    Se agrego metodo para limpiar log periodicamente y trazabilidad de la generación de codigo

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const base64url = require('base64url');
+const axios = require('axios');
+const winston = require('winston');
+require('winston-daily-rotate-file');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +13,27 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 const secretKey = process.env.SECRET_KEY;
+
+// Configuración de winston para rotación y eliminación de logs
+const transport = new winston.transports.DailyRotateFile({
+  filename: 'logs/application-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d' // Elimina los archivos de log después de 14 días
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.prettyPrint()
+  ),
+  transports: [
+    new winston.transports.Console(), // Para ver los logs en la consola
+    transport
+  ]
+});
 
 // Middleware de autenticación para el administrador
 function authenticateAdmin(req, res, next) {
@@ -20,6 +44,7 @@ function authenticateAdmin(req, res, next) {
     const [username, password] = decodedCredentials.split(':');
 
     if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
+      req.user = { username }; // Almacena el usuario autenticado
       return next();
     }
   }
@@ -32,12 +57,24 @@ function authenticate(req, res, next) {
   if (authHeader) {
     const token = authHeader.split(' ')[1];
     if (token === secretKey) {
+      req.user = { username: 'regular_user' }; // Almacena el usuario regular
       next();
     } else {
       res.status(403).json({ error: 'Forbidden - Token inválido.' });
     }
   } else {
     res.status(403).json({ error: 'Acceso denegado: Se requiere un token.' });
+  }
+}
+
+// Obtener la ubicación del usuario
+async function getClientLocation(ip) {
+  try {
+    const response = await axios.get(`http://ip-api.com/json/${ip}`);
+    return response.data;
+  } catch (error) {
+    logger.error({ message: `Error al obtener la ubicación del cliente: ${error.message}` });
+    return null;
   }
 }
 
@@ -90,6 +127,7 @@ function verifyAuthentication(req, res, next) {
       const [username, password] = decodedCredentials.split(':');
 
       if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
+        req.user = { username }; // Almacena el usuario autenticado
         return next();
       } else {
         authenticate(req, res, next);
@@ -101,7 +139,7 @@ function verifyAuthentication(req, res, next) {
 }
 
 // Endpoint para generar el token
-app.post('/generate-token', verifyAuthentication, (req, res) => {
+app.post('/generate-token', verifyAuthentication, async (req, res) => {
   const { data, environment } = req.body;
 
   if (!data || !environment) {
@@ -115,6 +153,16 @@ app.post('/generate-token', verifyAuthentication, (req, res) => {
   const token = createCustomJWT(data, secretKey);
   const tokenUrl = `${environments[environment.toLowerCase()]}${token}`;
   const environmentName = environmentNames[environment.toLowerCase()];
+
+  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const location = await getClientLocation(clientIp);
+
+  // Log en formato JSON con múltiples mensajes
+  logger.info({
+    'Token solicitado por el usuario': req.user ? req.user.username : 'Usuario no autenticado',
+    'Ubicación del cliente': location ? `${location.city}, ${location.regionName}, ${location.country}` : 'Ubicación no disponible',
+    'URL Generada para el ambiente': `${environmentName}: ${tokenUrl}`,
+  });
 
   res.json({
     [`URL Generada ${environmentName}:`]: tokenUrl
